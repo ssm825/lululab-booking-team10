725a0557de68163db21cd81831c454fbc42ec393
hoonjo1 <hoonjo1@gmail.com>
2022-10-17 05:36:37 +0900

    EDIT: 삭제

--- src/container/Main.tsx
+++ src/container/Main.tsx
@@ -1,22 +1,12 @@
-import React, { useEffect } from 'react';
-import { useSetRecoilState } from 'recoil';
+import React from 'react';
 import styled from 'styled-components';
-import useFetch from 'hooks/useFetch';
-import globalState from 'recoil/atom';
-import Loader from 'components/Loader';
+
 import Calendar from './Calendar';
 
 const Main = () => {
-  const { results, loading } = useFetch();
-  const setGlobalAtom = useSetRecoilState(globalState);
-
-  useEffect(() => {
-    setGlobalAtom(results);
-  }, [loading]);
-
   return (
     <Wapper>
-      {loading && results.length === 0 ? <Loader /> : <Calendar />}
+      <Calendar />
     </Wapper>
   );
 };
